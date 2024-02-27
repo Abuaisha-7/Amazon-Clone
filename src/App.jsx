6756011d7ddc447e946efc8ba3651fc5a93cec8b
Abuaisha-7 <heyrubedru8086@gmail.com>
2024-02-27 11:38:02 -1200

    Category added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/Carousel'
+import Categorys from './Components/Category/Category'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <CarouselEffect/>
+      <Categorys/>
     </>
   )
 }
---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {categoryInfos} from './CategoryFullInfo'
+import CategoryCard from './CategoryCard'
+import classes from './Category.module.css'
+
+const Categorys = () => {
+  return (
+    <section className={classes.category_contener}>
+        {
+            categoryInfos.map((info)=>(
+               <CategoryCard data = {info}/>
+               ))
+        }
+     
+    </section>
+  )
+}
+
+export default Categorys

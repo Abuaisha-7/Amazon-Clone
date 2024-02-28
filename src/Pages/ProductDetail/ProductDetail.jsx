3e8023a8aa1ea3f576e921d5eb14e7fe5408e5a5
Abuaisha-7 <heyrubedru8086@gmail.com>
2024-02-28 10:01:14 -1200

    routing is added

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from "./ProductDetail.module.css";
 import Layout from '../../Components/Layout/Layout'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/Products/ProductCard";
 
 const ProductDetail = () => {
+
+  const [product, setproduct] = useState([]);
+  const { productId } = useParams();
+  console.log(productId)
+
+  useEffect(() => {
+    axios.get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        console.log(res)
+        setproduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Layout>
-      <div>ProductDetail</div>
+     <ProductCard product={product}/>
     </Layout>
   )
 }
--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import classes from "./Results.module.css";
-import Layout from '../../Components/Layout/Layout'
+import Layout from "../../Components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoints";
+import ProductCard from "../../Components/Products/ProductCard";
 
 const Results = () => {
+  const [results, setresults] = useState([]);
+  const { catagoryName } = useParams();
+  // console.log(catagoryName)
+  useEffect(() => {
+    axios.get(`${productUrl}/products/category/${catagoryName}`)
+
+      .then((res) => {
+        console.log(res);
+        setresults(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Layout>
-      <div>Results</div>
+      <section>
+        <h1 style={{ padding: "30px" }}>Resualts</h1>
+        <p style={{ padding: "30px" }}>Catagory/{catagoryName}</p>
+        <hr />
+        <div className={classes.product_container}>
+          {results?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
